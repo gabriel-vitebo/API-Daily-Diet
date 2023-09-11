d9ff211ceda19da81dc2d34a0a24e98a8a5b67bb
gabriel-vitebo <vitebo@outlook.com>
2023-09-11 18:12:06 -0300

    creating regex to validate date and time

--- routes/mealMade.ts
+++ routes/mealMade.ts
@@ -16,14 +16,25 @@ export async function mealMadeRoutes(app: FastifyInstance) {
     const { name, description, date, hour, onDiet } =
       createMealBodySchema.parse(request.body)
 
-    await knex('mealMade').insert({
-      id: randomUUID(),
-      name,
-      description,
-      date,
-      hour,
-      onDiet,
-    })
+    const regexDate =
+      /^(0[1-9]|[12][0-9]|3[01])[./\-_](0[1-9]|1[0-2])[./\-_]\d{4}$/g
+
+    const regexTime = /^([01]\d|2[0-3]):[0-5]\d$/g
+
+    if (regexDate.test(date) && regexTime.test(hour)) {
+      await knex('mealMade').insert({
+        id: randomUUID(),
+        name,
+        description,
+        date,
+        hour,
+        onDiet,
+      })
+    } else {
+      throw new Error(
+        'please check that all data is in the correct format, remembering that the date can be in the format "dd/mm/yyyy", "dd.mm.yyyy", "dd-mm-yyyy" or "dd_mm_yy" and the time hh:mm (hours from 00 to 23 and minutes from 00 to 59)',
+      )
+    }
 
     return reply.status(201).send()
   })
